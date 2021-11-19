52e56df97913be876d14bd3b0387bd4d21db50b2
Trần Anh Tuấn <83346864+Tunasss@users.noreply.github.com>
2021-11-19 21:04:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,6 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    titleWeb: "CHÚC MỪNG 20-11",
+    introTitle: 'THẦY CÔ',
+    introDesc: `Dù học với thầy cô vài tháng, nhưng chúng em đã học rất vui, dù có ngày lớp quậy, lớp sôi nổi, lớp trầm, nhưng dù chỉ học qua màn ảnh nhỏ, bọn em vẫn rất yêu quý thầy cô, chúc thầy cô luôn mạnh khỏe, để có thể dìu dắt chúng em hết 3 năm cấp 3 này ạ, from 10 Tin with love <3`,
+    btnIntro: 'ẤN VÀO ĐÂY ĐỂ XEM BẤT NGỜ',
 }
